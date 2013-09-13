4c4c0f4808ce0bbf8c10eee3abdb1f0eb1998208
kalidasya <oliolart@gmail.com>
2013-09-13 16:39:25 +0200

    add check: disallow zero values with units

---
+++ css-checks/src/main/java/org/sonar/css/checks/DisallowUnitsForZeroValues.java
@@ -0,0 +1,52 @@
+/*
+ * Sonar CSS Plugin
+ * Copyright (C) 2013 Tamas Kende
+ * kende.tamas@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.css.checks;
+
+import com.sonar.sslr.api.AstNode;
+import com.sonar.sslr.squid.checks.SquidCheck;
+import org.sonar.check.BelongsToProfile;
+import org.sonar.check.Cardinality;
+import org.sonar.check.Priority;
+import org.sonar.check.Rule;
+import org.sonar.css.parser.CssGrammar;
+import org.sonar.sslr.parser.LexerlessGrammar;
+
+/**
+ * https://github.com/stubbornella/csslint/wiki/Disallow-universal-selector
+ * @author tkende
+ *
+ */
+@Rule(key = "universal-selector", priority = Priority.MAJOR, cardinality = Cardinality.SINGLE)
+@BelongsToProfile(title = CheckList.REPOSITORY_NAME, priority = Priority.MAJOR)
+public class DisallowUnitsForZeroValues extends SquidCheck<LexerlessGrammar> {
+
+  @Override
+  public void init() {
+    subscribeTo(CssGrammar.dimension, CssGrammar.percentage);
+  }
+
+  @Override
+  public void visitNode(AstNode astNode) {
+    if (astNode.getFirstChild(CssGrammar.number).getTokenValue().equals("0")) {
+      getContext().createLineViolation(this, "Disallow zero valus with units", astNode);
+    }
+  }
+
+}
---
+++ css-checks/src/test/java/org/sonar/css/checks/DisallowUnitsForZeroValuesTest.java
@@ -0,0 +1,44 @@
+/*
+ * Sonar CSS Plugin
+ * Copyright (C) 2013 Tamas Kende
+ * kende.tamas@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.css.checks;
+
+import org.sonar.css.CssAstScanner;
+
+import com.sonar.sslr.squid.checks.CheckMessagesVerifier;
+import org.junit.Test;
+import org.sonar.squid.api.SourceFile;
+
+import java.io.File;
+
+public class DisallowUnitsForZeroValuesTest {
+
+  @Test
+  public void test() {
+    DisallowUnitsForZeroValues check = new DisallowUnitsForZeroValues();
+    SourceFile file = CssAstScanner.scanSingleFile(new File(
+        "src/test/resources/checks/zerounits.css"), check);
+    CheckMessagesVerifier.verify(file.getCheckMessages()).next()
+        .atLine(2).withMessage("Disallow zero valus with units").next()
+        .atLine(6).withMessage("Disallow zero valus with units").next()
+        .atLine(10).withMessage("Disallow zero valus with units")
+        .noMore();
+  }
+
+}
