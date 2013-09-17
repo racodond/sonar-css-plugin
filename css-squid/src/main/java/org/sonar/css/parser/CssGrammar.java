00f237862e56fc5aabc5b2178ceae4abf1c9ec57
kalidasya <oliolart@gmail.com>
2013-09-17 14:48:41 +0200

    fix universal selectro match

--- css-squid/src/main/java/org/sonar/css/parser/CssGrammar.java
+++ css-squid/src/main/java/org/sonar/css/parser/CssGrammar.java
@@ -172,30 +172,30 @@ public enum CssGrammar implements GrammarRuleKey {
     b.rule(stylesheet).is(whiteSpaces, b.zeroOrMore(statement), eof);
     b.rule(statement).is(b.firstOf(atRule, ruleset)); // --> add sass variable declaration here:DONE
     b.rule(atRule).is(atkeyword,
-      b.zeroOrMore(any),
-      b.firstOf(
-        semiColon,
-        b.sequence(
-          lCurlyBracket,
-          b.zeroOrMore(
-            b.firstOf(ruleset, supDeclaration)),
-          rCurlyBracket)));
+        b.zeroOrMore(any),
+        b.firstOf(
+            semiColon,
+            b.sequence(
+                lCurlyBracket,
+                b.zeroOrMore(
+                    b.firstOf(ruleset, supDeclaration)),
+                rCurlyBracket)));
 
     b.rule(block).is(
-      lCurlyBracket,
-      b.optional(supDeclaration), // --> add sass variable declaration here:DONE
-      // --> add sass subruleset here:DONE
-      /*--> add nested properties (font: 2px/3px {
-      family: fantasy;
-      size: 30em;
-      weight: bold;
-      } here */
-      // --> add sass @extend, @import rule here:DONE
-      rCurlyBracket);
+        lCurlyBracket,
+        b.optional(supDeclaration), // --> add sass variable declaration here:DONE
+        // --> add sass subruleset here:DONE
+        /*--> add nested properties (font: 2px/3px {
+        family: fantasy;
+        size: 30em;
+        weight: bold;
+        } here */
+        // --> add sass @extend, @import rule here:DONE
+        rCurlyBracket);
     b.rule(ruleset).is(
-      b.optional(selector), // --> add sass parent selector '&' here:DONE
-      block
-      );
+        b.optional(selector), // --> add sass parent selector '&' here:DONE
+        block
+        );
 
     b.rule(selector).is(subSelector, b.zeroOrMore(b.firstOf(subSelector, addSpacing(",", b))));
     b.rule(subSelector).is(simpleSelector, b.zeroOrMore(combinators, simpleSelector));
@@ -204,12 +204,12 @@ public enum CssGrammar implements GrammarRuleKey {
     b.rule(childComb).is(addSpacing(">", b));
     b.rule(adjacentComb).is(addSpacing("+", b));
     b.rule(precededComb).is(addSpacing("~", b));
-    b.rule(simpleSelector).is(b.firstOf(typeSelector, universalSelector, otherSelector)).skip();
+    b.rule(simpleSelector).is(b.firstOf(universalSelector, typeSelector, otherSelector)).skip();
     b.rule(typeSelector).is(ident, b.zeroOrMore(subS));
     b.rule(universalSelector).is(
-      b.firstOf(
-        b.sequence(addSpacing(b.sequence("*", whiteSpace), b), b.zeroOrMore(subS)),
-        b.oneOrMore(subS)));
+        b.firstOf(
+            b.sequence(addSpacing("*", b), b.nextNot(ident), b.zeroOrMore(subS)),
+            b.oneOrMore(subS)));
     b.rule(otherSelector).is(any, b.zeroOrMore(subS));
 
     b.rule(subS).is(b.firstOf(attributeSelector, idSelector, classSelector, pseudo)).skip();
@@ -219,27 +219,27 @@ public enum CssGrammar implements GrammarRuleKey {
     b.rule(pseudo).is(colon, any);
 
     b.rule(supDeclaration).is(
-      declaration,
-      b.zeroOrMore(b.firstOf(semiColon, declaration))).skip();
+        declaration,
+        b.zeroOrMore(b.firstOf(semiColon, declaration))).skip();
 
     b.rule(declaration)
-      .is(property, colon, value);
+        .is(property, colon, value);
     b.rule(property).is(ident);
     b.rule(value).is(
-      b.oneOrMore(b.firstOf(any, block, atkeyword)));
+        b.oneOrMore(b.firstOf(any, block, atkeyword)));
     b.rule(any)
-      .is(
-        addSpacing(
-          b.firstOf(
-            function,
-            b.sequence(lParenthesis,
-              b.zeroOrMore(any),
-              rParenthesis),
-            b.sequence(lBracket,
-              b.zeroOrMore(any), rBracket),
-            percentage, dimension, string,
-            uri, hash, unicodeRange, includes, dashMatch,
-            ident, number, colon, important, delim), b)).skipIfOneChild();
+        .is(
+            addSpacing(
+                b.firstOf(
+                    function,
+                    b.sequence(lParenthesis,
+                        b.zeroOrMore(any),
+                        rParenthesis),
+                    b.sequence(lBracket,
+                        b.zeroOrMore(any), rBracket),
+                    percentage, dimension, string,
+                    uri, hash, unicodeRange, includes, dashMatch,
+                    ident, number, colon, important, delim), b)).skipIfOneChild();
     // b.rule(unused).is(
     // b.firstOf(block, b.sequence(atkeyword, whiteSpaces),
     // b.sequence(semiColon, whiteSpaces)/*
@@ -263,15 +263,15 @@ public enum CssGrammar implements GrammarRuleKey {
     b.rule(number).is(addSpacing(_num, b));
     b.rule(percentage).is(addSpacing(b.sequence(number, "%"), b));
     b.rule(dimension).is(addSpacing(b.sequence(number, unit), b));
-    b.rule(unit).is(b.firstOf("em","ex","ch","rem","vw","vh","vmin","vmax","cm","mm","in","px","pt","pc"));
+    b.rule(unit).is(b.firstOf("em", "ex", "ch", "rem", "vw", "vh", "vmin", "vmax", "cm", "mm", "in", "px", "pt", "pc"));
     b.rule(uri).is(
-      addSpacing(
-        b.firstOf(b.sequence("url(", _w, string, _w, rParenthesis), b
-          .sequence("url(", _w, b.zeroOrMore(b.firstOf(
-            b.regexp("[!#$%&*-\\[\\]-~]"), _nonascii,
-            _escape)), _w, rParenthesis)), b));
+        addSpacing(
+            b.firstOf(b.sequence("url(", _w, string, _w, rParenthesis), b
+                .sequence("url(", _w, b.zeroOrMore(b.firstOf(
+                    b.regexp("[!#$%&*-\\[\\]-~]"), _nonascii,
+                    _escape)), _w, rParenthesis)), b));
     b.rule(unicodeRange)
-      .is(addSpacing(b.regexp("u\\+[0-9a-f?]{1,6}(-[0-9a-f]{1,6})?"), b));
+        .is(addSpacing(b.regexp("u\\+[0-9a-f?]{1,6}(-[0-9a-f]{1,6})?"), b));
     // b.rule(cdo).is("<!--");
     // b.rule(cdc).is("-->");
     b.rule(colon).is(addSpacing(":", b));
@@ -285,13 +285,13 @@ public enum CssGrammar implements GrammarRuleKey {
     b.rule(comma).is(addSpacing(",", b));
     b.rule(whiteSpace).is(b.regexp(WHITESPACE)).skip();
     b.rule(whiteSpaces).is(b.zeroOrMore(
-      b.firstOf(
-        b.skippedTrivia(whiteSpace),
-        b.commentTrivia(b.regexp(CssLexer.COMMENT)), // --> add sass // comment here
-        b.commentTrivia(b.regexp(CssLexer.COMMENT2))))).skip();
+        b.firstOf(
+            b.skippedTrivia(whiteSpace),
+            b.commentTrivia(b.regexp(CssLexer.COMMENT)), // --> add sass // comment here
+            b.commentTrivia(b.regexp(CssLexer.COMMENT2))))).skip();
     b.rule(comment).is(b.regexp("\\/\\*[^*]*\\*+([^/*][^*]*\\*+)*\\/"));
     b.rule(function).is(addSpacing(b.sequence(ident, lParenthesis), b), b.zeroOrMore(parameters),
-      rParenthesis);
+        rParenthesis);
     b.rule(parameters).is(parameter, b.zeroOrMore(comma, parameter));
     b.rule(parameter).is(b.oneOrMore(any));
     b.rule(includes).is(addSpacing("~=", b));
@@ -314,43 +314,43 @@ public enum CssGrammar implements GrammarRuleKey {
     b.rule(_ident).is(b.token(GenericTokenType.IDENTIFIER, b.sequence(_nmstart, b.zeroOrMore(_nmchar)))).skip();
     b.rule(_name).is(b.token(GenericTokenType.LITERAL, b.oneOrMore(_nmchar))).skip();
     b.rule(_nmstart).is(
-      b.firstOf(b.regexp(NMSTART), "-", "*", _nonascii, _escape)).skip();
+        b.firstOf(b.regexp(NMSTART), "-", "*", _nonascii, _escape)).skip();
     b.rule(_nonascii).is(b.regexp(NONASCII)).skip();
     b.rule(_unicode).is(
-      b.regexp("\\\\[0-9a-f]{1,6}(\\r\\n|[ \\n\\r\\t\\f])?")).skip();
+        b.regexp("\\\\[0-9a-f]{1,6}(\\r\\n|[ \\n\\r\\t\\f])?")).skip();
     b.rule(_escape).is(
-      b.firstOf(_unicode, b.regexp("\\\\[^\\n\\r\\f0-9a-f]"))).skip();
+        b.firstOf(_unicode, b.regexp("\\\\[^\\n\\r\\f0-9a-f]"))).skip();
     b.rule(_nmchar).is(
-      b.firstOf(b.regexp(NMCHAR), _nonascii, _escape)).skip();
+        b.firstOf(b.regexp(NMCHAR), _nonascii, _escape)).skip();
     b.rule(_num).is(b.token(GenericTokenType.LITERAL, b.sequence(b.optional("-"), // '-' IS NOT DEFINED IN THE W3 spec
-      b.firstOf(b.regexp("[0-9]*\\.[0-9]+"), b.regexp("[0-9]+"))))).skip();
+        b.firstOf(b.regexp("[0-9]*\\.[0-9]+"), b.regexp("[0-9]+"))))).skip();
     b.rule(_string).is(b.token(GenericTokenType.LITERAL, b.firstOf(_string1, _string2))).skip();
     b.rule(_string1).is(
-      "\"",
-      b.zeroOrMore(b.firstOf(b.regexp("[^\\n\\r\\f\\\\\"]"),
-        b.sequence("\\", _nl), _escape)), "\"").skip();
+        "\"",
+        b.zeroOrMore(b.firstOf(b.regexp("[^\\n\\r\\f\\\\\"]"),
+            b.sequence("\\", _nl), _escape)), "\"").skip();
     b.rule(_string2).is(
-      "'",
-      b.zeroOrMore(b.firstOf(b.regexp("[^\\n\\r\\f\\\\']"),
-        b.sequence("\\", _nl), _escape)), "'").skip();
+        "'",
+        b.zeroOrMore(b.firstOf(b.regexp("[^\\n\\r\\f\\\\']"),
+            b.sequence("\\", _nl), _escape)), "'").skip();
     b.rule(_badString).is(b.firstOf(_badString1, _badString2)).skip();
     b.rule(_badString1).is(
-      "\"",
-      b.zeroOrMore(b.regexp("[^\\n\\r\\f\\\\\"]"),
-        b.sequence("\\", _nl), _escape), "\"").skip();
+        "\"",
+        b.zeroOrMore(b.regexp("[^\\n\\r\\f\\\\\"]"),
+            b.sequence("\\", _nl), _escape), "\"").skip();
     b.rule(_badString2).is(
-      "'",
-      b.zeroOrMore(b.regexp("[^\\n\\r\\f\\\\\']"),
-        b.sequence("\\", _nl), _escape), "'").skip();
+        "'",
+        b.zeroOrMore(b.regexp("[^\\n\\r\\f\\\\\']"),
+            b.sequence("\\", _nl), _escape), "'").skip();
     b.rule(_badcomment).is(b.firstOf(_badcomment1, _badcomment2)).skip();
     b.rule(_badcomment1).is(b.regexp("\\/\\*[^*]*\\*+([^/*][^*]*\\*+)*")).skip();
     b.rule(_badcomment2).is(b.regexp("\\/\\*[^*]*(\\*+[^/*][^*]*)*")).skip();
     b.rule(_baduri).is(b.firstOf(_baduri1, _baduri2, _baduri3)).skip();
     b.rule(_baduri1).is(
-      "url(",
-      _w,
-      b.zeroOrMore(b.firstOf(b.regexp("[!#$%&*-~]"), _nonascii,
-        _escape)), _w).skip();
+        "url(",
+        _w,
+        b.zeroOrMore(b.firstOf(b.regexp("[!#$%&*-~]"), _nonascii,
+            _escape)), _w).skip();
     b.rule(_baduri2).is("url(", _w, _string, _w).skip();
     b.rule(_baduri3).is("url(", _w, _badString).skip();
     b.rule(_nl).is(b.firstOf("\n", "\r\n", "\r", "\f")).skip();
