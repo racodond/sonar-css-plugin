9062eb40eb52c06dbe9fe87e65c5b9ea9470dceb
David RACODON <david.racodon@gmail.com>
2015-05-12 23:20:47 +0200

    Upgrade to SSLR Squid Bridge 2.6 following documentation at http://docs.sonarqube.org/display/DEV/SSLR+Squid+Bridge

--- sonar-css-plugin/src/main/java/org/sonar/plugins/css/CssCommonRulesEngine.java
+++ sonar-css-plugin/src/main/java/org/sonar/plugins/css/CssCommonRulesEngine.java
@@ -19,8 +19,8 @@
  */
 package org.sonar.plugins.css;
 
-import org.sonar.commonrules.api.CommonRulesEngine;
-import org.sonar.commonrules.api.CommonRulesRepository;
+import org.sonar.squidbridge.commonrules.api.CommonRulesEngine;
+import org.sonar.squidbridge.commonrules.api.CommonRulesRepository;
 import org.sonar.plugins.css.core.Css;
 
 public class CssCommonRulesEngine extends CommonRulesEngine {
