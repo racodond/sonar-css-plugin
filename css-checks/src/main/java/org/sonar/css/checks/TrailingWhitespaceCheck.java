39a79493d88f1183fba219bcfde96ee9c856f5d6
David RACODON <david.racodon@gmail.com>
2015-05-18 10:39:55 +0200

    Add new rule: S1131 - Lines should not end with trailing whitespaces

---
+++ css-checks/src/main/java/org/sonar/css/checks/TrailingWhitespaceCheck.java
@@ -0,0 +1,71 @@
+/*
+ * SonarQube CSS Plugin
+ * Copyright (C) 2013 Tamas Kende
+ * kende.tamas@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.css.checks;
+
+import com.google.common.io.Files;
+import com.sonar.sslr.api.AstNode;
+import org.sonar.api.server.rule.RulesDefinition;
+import org.sonar.api.utils.SonarException;
+import org.sonar.check.Priority;
+import org.sonar.check.Rule;
+import org.sonar.css.CharsetAwareVisitor;
+import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
+import org.sonar.squidbridge.annotations.SqaleSubCharacteristic;
+import org.sonar.squidbridge.checks.SquidCheck;
+import org.sonar.sslr.parser.LexerlessGrammar;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.regex.Pattern;
+
+@Rule(
+  key = "S1131",
+  name = "Lines should not end with trailing whitespaces",
+  priority = Priority.MINOR,
+  tags = {Tags.CONVENTION})
+@SqaleSubCharacteristic(RulesDefinition.SubCharacteristics.READABILITY)
+@SqaleConstantRemediation("1min")
+public class TrailingWhitespaceCheck extends SquidCheck<LexerlessGrammar> implements CharsetAwareVisitor {
+
+  private static final String WHITESPACE = "\\t\\u000B\\f\\u0020\\u00A0\\uFEFF\\p{Zs}";
+  private Charset charset;
+
+  @Override
+  public void setCharset(Charset charset) {
+    this.charset = charset;
+  }
+
+  @Override
+  public void visitFile(AstNode astNode) {
+    List<String> lines;
+    try {
+      lines = Files.readLines(getContext().getFile(), charset);
+    } catch (IOException e) {
+      throw new SonarException(e);
+    }
+    for (int i = 0; i < lines.size(); i++) {
+      String line = lines.get(i);
+      if (line.length() > 0 && Pattern.matches("[" + WHITESPACE + "]", line.subSequence(line.length() - 1, line.length()))) {
+        getContext().createLineViolation(this, "Remove the useless trailing whitespaces at the end of this line.", i + 1);
+      }
+    }
+  }
+}
---
+++ css-checks/src/test/java/org/sonar/css/checks/TrailingWhitespaceCheckTest.java
@@ -0,0 +1,49 @@
+/*
+ * SonarQube CSS Plugin
+ * Copyright (C) 2013 Tamas Kende
+ * kende.tamas@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.css.checks;
+
+import org.junit.Test;
+import org.sonar.css.CssAstScanner;
+import org.sonar.squidbridge.api.SourceFile;
+import org.sonar.squidbridge.checks.CheckMessagesVerifier;
+
+import java.io.File;
+
+public class TrailingWhitespaceCheckTest {
+
+  private TrailingWhitespaceCheck check = new TrailingWhitespaceCheck();
+
+  @Test
+  public void should_find_trailing_whitespaces_and_raise_issues() {
+    SourceFile testFile = CssAstScanner.scanSingleFile(new File("src/test/resources/checks/trailingWhitespace.css"), check);
+    CheckMessagesVerifier.verify(testFile.getCheckMessages()).next()
+      .atLine(1).withMessage("Remove the useless trailing whitespaces at the end of this line.").next()
+      .atLine(4).withMessage("Remove the useless trailing whitespaces at the end of this line.").next()
+      .atLine(6).withMessage("Remove the useless trailing whitespaces at the end of this line.")
+      .noMore();
+  }
+
+  @Test
+  public void should_not_find_trailing_whitespaces_and_not_raise_issues() {
+    SourceFile testFile = CssAstScanner.scanSingleFile(new File("src/test/resources/checks/emptyRule.css"), check);
+    CheckMessagesVerifier.verify(testFile.getCheckMessages()).noMore();
+  }
+
+}
