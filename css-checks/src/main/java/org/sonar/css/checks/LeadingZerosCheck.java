3801941cf10331c2a5186d9097d052132137c08b
David RACODON <david.racodon@gmail.com>
2015-05-19 16:38:57 +0200

    Add a rule: Leading zeros should be removed

---
+++ css-checks/src/main/java/org/sonar/css/checks/LeadingZerosCheck.java
@@ -0,0 +1,55 @@
+/*
+ * SonarQube CSS Plugin
+ * Copyright (C) 2013 Tamas Kende
+ * kende.tamas@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.css.checks;
+
+import com.sonar.sslr.api.AstNode;
+import org.sonar.api.server.rule.RulesDefinition;
+import org.sonar.check.Priority;
+import org.sonar.check.Rule;
+import org.sonar.css.parser.CssGrammar;
+import org.sonar.squidbridge.annotations.ActivatedByDefault;
+import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
+import org.sonar.squidbridge.annotations.SqaleSubCharacteristic;
+import org.sonar.squidbridge.checks.SquidCheck;
+import org.sonar.sslr.parser.LexerlessGrammar;
+
+@Rule(
+  key = "leading-zeros",
+  name = "Leading zeros should be removed",
+  priority = Priority.MINOR,
+  tags = {Tags.CONVENTION})
+@ActivatedByDefault
+@SqaleSubCharacteristic(RulesDefinition.SubCharacteristics.READABILITY)
+@SqaleConstantRemediation("2min")
+public class LeadingZerosCheck extends SquidCheck<LexerlessGrammar> {
+
+  @Override
+  public void init() {
+    subscribeTo(CssGrammar.DECLARATION);
+  }
+
+  @Override
+  public void leaveNode(AstNode astNode) {
+    if (astNode.getFirstChild(CssGrammar.VALUE).getTokenValue().startsWith("0.")) {
+      getContext().createLineViolation(this, "Remove this leading zero", astNode);
+    }
+  }
+
+}
---
+++ css-checks/src/test/java/org/sonar/css/checks/LeadingZerosCheckTest.java
@@ -0,0 +1,40 @@
+/*
+ * SonarQube CSS Plugin
+ * Copyright (C) 2013 Tamas Kende
+ * kende.tamas@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.css.checks;
+
+import org.junit.Test;
+import org.sonar.css.CssAstScanner;
+import org.sonar.squidbridge.api.SourceFile;
+import org.sonar.squidbridge.checks.CheckMessagesVerifier;
+
+import java.io.File;
+
+public class LeadingZerosCheckTest {
+
+  private final static String MESSAGE = "Remove this leading zero";
+  private LeadingZerosCheck check = new LeadingZerosCheck();
+
+  @Test
+  public void should_find_some_leading_zeros() {
+    SourceFile file = CssAstScanner.scanSingleFile(new File("src/test/resources/checks/leadingZeros.css"), check);
+    CheckMessagesVerifier.verify(file.getCheckMessages()).next().atLine(3).withMessage(MESSAGE).noMore();
+  }
+
+}
