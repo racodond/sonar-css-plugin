1b24024b2e3d8565333326178f89f262b0c1cbb3
David RACODON <david.racodon@gmail.com>
2015-06-02 15:14:06 +0200

    Add rule: "!important" annotation should be placed at the end of the declaration

---
+++ css-checks/src/main/java/org/sonar/css/checks/ImportantPositionCheck.java
@@ -0,0 +1,59 @@
+/*
+ * SonarQube CSS Plugin
+ * Copyright (C) 2013 Tamas Kende
+ * kende.tamas@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.css.checks;
+
+import com.sonar.sslr.api.AstNode;
+import org.sonar.api.server.rule.RulesDefinition;
+import org.sonar.check.Priority;
+import org.sonar.check.Rule;
+import org.sonar.css.parser.CssGrammar;
+import org.sonar.squidbridge.annotations.ActivatedByDefault;
+import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
+import org.sonar.squidbridge.annotations.SqaleSubCharacteristic;
+import org.sonar.squidbridge.checks.SquidCheck;
+import org.sonar.sslr.parser.LexerlessGrammar;
+
+@Rule(
+  key = "important-position",
+  name = "\"!important\" annotation should be placed at the end of the declaration",
+  priority = Priority.CRITICAL,
+  tags = {Tags.BUG})
+@ActivatedByDefault
+@SqaleSubCharacteristic(RulesDefinition.SubCharacteristics.INSTRUCTION_RELIABILITY)
+@SqaleConstantRemediation("5min")
+public class ImportantPositionCheck extends SquidCheck<LexerlessGrammar> {
+
+  @Override
+  public void init() {
+    subscribeTo(CssGrammar.IMPORTANT);
+  }
+
+  @Override
+  public void leaveNode(AstNode astNode) {
+    if (astNode.getNextSibling() != null) {
+      getContext().createLineViolation(
+        this,
+        "Move the \"!important\" annotation to the end of the declaration, just before the semi-colon.",
+        astNode
+        );
+    }
+  }
+
+}
