71430ba82ebe4cdcfa3cfcfc2591b01f3d10eb40
David RACODON <david.racodon@gmail.com>
2015-06-08 08:57:40 +0200

    Issue 84 - Add rule: Unknown @-rules should be removed

---
+++ css-checks/src/main/java/org/sonar/css/checks/UnknownAtRulesCheck.java
@@ -0,0 +1,56 @@
+/*
+ * SonarQube CSS Plugin
+ * Copyright (C) 2013 Tamas Kende
+ * kende.tamas@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.css.checks;
+
+import com.sonar.sslr.api.AstNode;
+import org.sonar.api.server.rule.RulesDefinition;
+import org.sonar.check.Priority;
+import org.sonar.check.Rule;
+import org.sonar.css.checks.utils.CssAtRules;
+import org.sonar.css.parser.CssGrammar;
+import org.sonar.squidbridge.annotations.ActivatedByDefault;
+import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
+import org.sonar.squidbridge.annotations.SqaleSubCharacteristic;
+import org.sonar.squidbridge.checks.SquidCheck;
+import org.sonar.sslr.parser.LexerlessGrammar;
+
+@Rule(
+  key = "unknown-at-rules",
+  name = "Unknown @-rules should be removed",
+  priority = Priority.MAJOR,
+  tags = {Tags.PITFALL})
+@SqaleSubCharacteristic(RulesDefinition.SubCharacteristics.LOGIC_RELIABILITY)
+@SqaleConstantRemediation("10min")
+@ActivatedByDefault
+public class UnknownAtRulesCheck extends SquidCheck<LexerlessGrammar> {
+
+  @Override
+  public void init() {
+    subscribeTo(CssGrammar.AT_KEYWORD);
+  }
+
+  @Override
+  public void leaveNode(AstNode astNode) {
+    if (!CssAtRules.CSS_AT_RULES.contains(astNode.getFirstChild(CssGrammar.IDENT).getTokenValue().toLowerCase())) {
+      getContext().createLineViolation(this, "Remove this usage of the unknown \"{0}\" CSS @-rule.", astNode, astNode.getFirstChild(CssGrammar.IDENT).getTokenValue());
+    }
+  }
+
+}
