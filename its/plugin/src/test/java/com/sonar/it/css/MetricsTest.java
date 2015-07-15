f670d5b8c771b85af6574536a0f935b478f0fee6
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2015-07-15 15:43:55 +0200

    Add integration tests

---
+++ its/plugin/src/test/java/com/sonar/it/css/MetricsTest.java
@@ -0,0 +1,139 @@
+/*
+ * SonarSource :: CSS :: ITs :: Plugin
+ * Copyright (C) 2014 ${owner}
+ * sonarqube@googlegroups.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package com.sonar.it.css;
+
+import com.sonar.orchestrator.Orchestrator;
+import com.sonar.orchestrator.build.SonarRunner;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.sonar.wsclient.Sonar;
+import org.sonar.wsclient.services.Measure;
+import org.sonar.wsclient.services.Resource;
+import org.sonar.wsclient.services.ResourceQuery;
+
+import java.io.File;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class MetricsTest {
+
+  @ClassRule
+  public static Orchestrator orchestrator = Tests.ORCHESTRATOR;
+
+  private static final String PROJECT_KEY = "metrics";
+  private static final String FILE_NAME = "source1.css";
+  private static Sonar wsClient;
+
+  @BeforeClass
+  public static void init() {
+    orchestrator.resetData();
+
+    SonarRunner build = Tests.createSonarRunner()
+      .setProjectDir(new File("projects/metrics/"))
+      .setProjectKey(PROJECT_KEY)
+      .setProjectName(PROJECT_KEY)
+      .setProjectVersion("1.0")
+      .setSourceDirs("src")
+      .setProperty("sonar.sourceEncoding", "UTF-8")
+      .setProfile("empty-profile");
+    orchestrator.executeBuild(build);
+
+    wsClient = orchestrator.getServer().getWsClient();
+  }
+
+  @Test
+  public void project_level() {
+    // Size
+    assertThat(getProjectMeasure("ncloc").getIntValue()).isEqualTo(18);
+    assertThat(getProjectMeasure("lines").getIntValue()).isEqualTo(23);
+    assertThat(getProjectMeasure("files").getIntValue()).isEqualTo(2);
+    assertThat(getProjectMeasure("directories").getIntValue()).isEqualTo(1);
+    if (Tests.is_at_least_plugin_1_2()) {
+      assertThat(getProjectMeasure("functions")).isNull();
+    }
+    assertThat(getProjectMeasure("statements").getIntValue()).isEqualTo(12);
+    // Documentation
+    assertThat(getProjectMeasure("comment_lines").getIntValue()).isEqualTo(0);
+    assertThat(getProjectMeasure("commented_out_code_lines")).isNull();
+    assertThat(getProjectMeasure("comment_lines_density").getValue()).isEqualTo(0.0);
+    assertThat(getProjectMeasure("public_documented_api_density")).isNull();
+    // Complexity
+    assertThat(getProjectMeasure("complexity")).isNull();
+    assertThat(getProjectMeasure("function_complexity")).isNull();
+    assertThat(getProjectMeasure("function_complexity_distribution")).isNull();
+    assertThat(getProjectMeasure("file_complexity")).isNull();
+    assertThat(getProjectMeasure("file_complexity_distribution")).isNull();
+    // Duplication
+    assertThat(getProjectMeasure("duplicated_lines").getValue()).isEqualTo(0.0);
+    assertThat(getProjectMeasure("duplicated_blocks").getValue()).isEqualTo(0.0);
+    assertThat(getProjectMeasure("duplicated_files").getValue()).isEqualTo(0.0);
+    assertThat(getProjectMeasure("duplicated_lines_density").getValue()).isEqualTo(0.0);
+    // Rules
+    assertThat(getProjectMeasure("violations").getValue()).isEqualTo(0.0);
+    // Tests
+    assertThat(getProjectMeasure("tests")).isNull();
+    assertThat(getProjectMeasure("coverage")).isNull();
+  }
+
+  @Test
+  public void file_level() {
+    // Size
+    assertThat(getFileMeasure("ncloc").getIntValue()).isEqualTo(14);
+    assertThat(getFileMeasure("lines").getIntValue()).isEqualTo(18);
+    if (Tests.is_at_least_plugin_1_2()) {
+      assertThat(getFileMeasure("functions")).isNull();
+    }
+    assertThat(getFileMeasure("statements").getIntValue()).isEqualTo(9);
+    // Documentation
+    assertThat(getFileMeasure("comment_lines").getIntValue()).isEqualTo(0);
+    assertThat(getFileMeasure("commented_out_code_lines")).isNull();
+    assertThat(getFileMeasure("comment_lines_density").getValue()).isEqualTo(0.0);
+    // Complexity
+    assertThat(getFileMeasure("complexity")).isNull();
+    assertThat(getFileMeasure("function_complexity")).isNull();
+    assertThat(getFileMeasure("function_complexity_distribution")).isNull();
+    assertThat(getFileMeasure("file_complexity")).isNull();
+
+    // Duplication
+    assertThat(getFileMeasure("duplicated_lines")).isNull();
+    assertThat(getFileMeasure("duplicated_blocks")).isNull();
+    assertThat(getFileMeasure("duplicated_files")).isNull();
+    assertThat(getFileMeasure("duplicated_lines_density")).isNull();
+    // Rules
+    assertThat(getFileMeasure("violations")).isNull();
+  }
+
+  /* Helper methods */
+
+  private Measure getProjectMeasure(String metricKey) {
+    Resource resource = wsClient.find(ResourceQuery.createForMetrics(PROJECT_KEY, metricKey));
+    return resource == null ? null : resource.getMeasure(metricKey);
+  }
+
+  private Measure getFileMeasure(String metricKey) {
+    Resource resource = wsClient.find(ResourceQuery.createForMetrics(keyFor(FILE_NAME), metricKey));
+    return resource == null ? null : resource.getMeasure(metricKey);
+  }
+
+  private static String keyFor(String s) {
+    return PROJECT_KEY + (orchestrator.getConfiguration().getSonarVersion().isGreaterThanOrEquals("4.2") ? ":src/" : ":") + s;
+  }
+}
---
+++ its/plugin/src/test/java/com/sonar/it/css/Tests.java
@@ -0,0 +1,66 @@
+/*
+ * SonarSource :: CSS :: ITs :: Plugin
+ * Copyright (C) 2014 ${owner}
+ * sonarqube@googlegroups.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package com.sonar.it.css;
+
+import com.sonar.orchestrator.Orchestrator;
+import com.sonar.orchestrator.build.SonarRunner;
+import com.sonar.orchestrator.locator.FileLocation;
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  MetricsTest.class,
+  IssueTest.class,
+})
+public class Tests {
+
+  private static final String PLUGIN_KEY = "css";
+
+  @ClassRule
+  public static final Orchestrator ORCHESTRATOR = Orchestrator.builderEnv()
+    .addPlugin(PLUGIN_KEY)
+    .setMainPluginKey(PLUGIN_KEY)
+    .restoreProfileAtStartup(FileLocation.ofClasspath("/com/sonar/it/css/it-profile.xml"))
+    .restoreProfileAtStartup(FileLocation.ofClasspath("/com/sonar/it/css/empty-profile.xml"))
+    .build();
+
+  public static SonarRunner createSonarRunner() {
+    SonarRunner build = SonarRunner.create();
+    if (!is_multi_language()) {
+      build.setProperty("sonar.language", "css");
+    }
+    return build;
+  }
+
+  private static boolean is_multi_language() {
+    return is_after_sonar_4_2();
+  }
+
+  public static boolean is_after_sonar_4_2() {
+    return ORCHESTRATOR.getConfiguration().getSonarVersion().isGreaterThanOrEquals("4.2");
+  }
+
+  public static boolean is_at_least_plugin_1_2() {
+    return ORCHESTRATOR.getConfiguration().getPluginVersion(PLUGIN_KEY).isGreaterThanOrEquals("1.2");
+  }
+
+}
