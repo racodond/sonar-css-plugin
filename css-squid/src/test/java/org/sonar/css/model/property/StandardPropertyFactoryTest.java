853d72d1cbc2d2aefd8a05750df9f9b65d307990
David RACODON <david.racodon@gmail.com>
2016-06-08 17:38:33 +0200

    Fix #227 Add 'mask-mode' property

--- css-squid/src/test/java/org/sonar/css/model/property/StandardPropertyFactoryTest.java
+++ css-squid/src/test/java/org/sonar/css/model/property/StandardPropertyFactoryTest.java
@@ -93,7 +93,7 @@ public class StandardPropertyFactoryTest {
 
   @Test
   public void number_of_standard_properties() {
-    assertEquals(446, StandardPropertyFactory.createAll().size());
+    assertEquals(447, StandardPropertyFactory.createAll().size());
   }
 
 }
