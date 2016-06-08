55c72a8e105cb6706e9c354b29b53050b7fee5c0
David RACODON <david.racodon@gmail.com>
2016-06-08 17:43:33 +0200

    Fix #231 Add 'mask-origin' property

--- css-squid/src/test/java/org/sonar/css/model/property/StandardPropertyFactoryTest.java
+++ css-squid/src/test/java/org/sonar/css/model/property/StandardPropertyFactoryTest.java
@@ -93,7 +93,7 @@ public class StandardPropertyFactoryTest {
 
   @Test
   public void number_of_standard_properties() {
-    assertEquals(450, StandardPropertyFactory.createAll().size());
+    assertEquals(451, StandardPropertyFactory.createAll().size());
   }
 
 }
