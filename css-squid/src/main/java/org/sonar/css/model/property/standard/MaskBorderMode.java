7ff179de48ccc82d90c6a143ea09809d69777f7c
David RACODON <david.racodon@gmail.com>
2016-06-08 17:56:35 +0200

    Fix #235 Add 'mask-border-mode' property

--- css-squid/src/main/java/org/sonar/css/model/property/standard/MaskBorderMode.java
+++ css-squid/src/main/java/org/sonar/css/model/property/standard/MaskBorderMode.java
@@ -17,32 +17,17 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-package org.sonar.css.checks;
+package org.sonar.css.model.property.standard;
 
-import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import org.sonar.css.model.property.StandardProperty;
+import org.sonar.css.model.property.validator.valueelement.IdentifierValidator;
 
-import java.io.File;
+public class MaskBorderMode extends StandardProperty {
 
-import junit.framework.Assert;
-import org.junit.Test;
-import org.sonar.css.checks.verifier.CssCheckVerifier;
-
-public class ValidatePropertyValueCheckTest {
-
-  @Test
-  public void test_validate_properties() {
-    File testDirectory = new File("src/test/resources/checks/properties");
-    testDirectory = Preconditions.checkNotNull(testDirectory);
-    for (File file : testDirectory.listFiles()) {
-      CssCheckVerifier.verify(new ValidatePropertyValueCheck(), file);
-    }
-  }
-
-  @Test
-  public void test_number_of_validated_properties() {
-    File testDirectory = new File("src/test/resources/checks/properties");
-    testDirectory = Preconditions.checkNotNull(testDirectory);
-    Assert.assertEquals(246, testDirectory.listFiles().length);
+  public MaskBorderMode() {
+    addLinks("https://drafts.fxtf.org/masking/#propdef-mask-border-mode");
+    addValidators(new IdentifierValidator(ImmutableList.of("luminance", "alpha")));
   }
 
 }
--- css-squid/src/test/java/org/sonar/css/model/property/StandardPropertyFactoryTest.java
+++ css-squid/src/test/java/org/sonar/css/model/property/StandardPropertyFactoryTest.java
@@ -93,7 +93,7 @@ public class StandardPropertyFactoryTest {
 
   @Test
   public void number_of_standard_properties() {
-    assertEquals(454, StandardPropertyFactory.createAll().size());
+    assertEquals(455, StandardPropertyFactory.createAll().size());
   }
 
 }
