b7820252ffc097083c22549aaf6251030f15fffe
David RACODON <david.racodon@gmail.com>
2016-06-08 17:41:12 +0200

    Fix #229 Add 'mask-position' property

--- css-squid/src/test/java/org/sonar/css/model/property/StandardPropertyFactoryTest.java
+++ css-squid/src/test/java/org/sonar/css/model/property/StandardPropertyFactoryTest.java
@@ -93,7 +93,7 @@ public class StandardPropertyFactoryTest {
 
   @Test
   public void number_of_standard_properties() {
-    assertEquals(448, StandardPropertyFactory.createAll().size());
+    assertEquals(449, StandardPropertyFactory.createAll().size());
   }
 
 }
