38ab7e97f3b5e2d9ac4538fe6fb925fc0982f981
David RACODON <david.racodon@gmail.com>
2016-06-08 17:22:13 +0200

    Fix #225 Add 'text-combine-upright' property and its validator

--- css-squid/src/main/java/org/sonar/css/model/property/standard/TextCombineUpright.java
+++ css-squid/src/main/java/org/sonar/css/model/property/standard/TextCombineUpright.java
@@ -17,32 +17,16 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-package org.sonar.css.checks;
+package org.sonar.css.model.property.standard;
 
-import com.google.common.base.Preconditions;
+import org.sonar.css.model.property.StandardProperty;
+import org.sonar.css.model.property.validator.property.TextCombineUprightValidator;
 
-import java.io.File;
+public class TextCombineUpright extends StandardProperty {
 
-import junit.framework.Assert;
-import org.junit.Test;
-import org.sonar.css.checks.verifier.CssCheckVerifier;
-
-public class ValidatePropertyValueCheckTest {
-
-  @Test
-  public void test_validate_properties() {
-    File testDirectory = new File("src/test/resources/checks/properties");
-    testDirectory = Preconditions.checkNotNull(testDirectory);
-    for (File file : testDirectory.listFiles()) {
-      CssCheckVerifier.verify(new ValidatePropertyValueCheck(), file);
-    }
-  }
-
-  @Test
-  public void test_number_of_validated_properties() {
-    File testDirectory = new File("src/test/resources/checks/properties");
-    testDirectory = Preconditions.checkNotNull(testDirectory);
-    Assert.assertEquals(244, testDirectory.listFiles().length);
+  public TextCombineUpright() {
+    addLinks("https://drafts.csswg.org/css-writing-modes-3/#propdef-text-combine-upright");
+    addValidators(new TextCombineUprightValidator());
   }
 
 }
