3ed228c4587409f9dfb8bd089196f2b807993e96
David RACODON <david.racodon@gmail.com>
2016-06-09 14:48:52 +0200

    Fix #247 Add 'fallback' property

--- css-squid/src/test/java/org/sonar/css/model/property/StandardPropertyFactoryTest.java
+++ css-squid/src/test/java/org/sonar/css/model/property/StandardPropertyFactoryTest.java
@@ -93,7 +93,7 @@ public class StandardPropertyFactoryTest {
 
   @Test
   public void number_of_standard_properties() {
-    assertEquals(466, StandardPropertyFactory.createAll().size());
+    assertEquals(467, StandardPropertyFactory.createAll().size());
   }
 
 }
