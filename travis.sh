5a5de3e9d2532aeb0b1e275369fd5ea7c4b898af
David RACODON <david.racodon@gmail.com>
2016-07-29 15:08:53 +0200

    Add SonarQube analysis

--- travis.sh
+++ travis.sh
@@ -1,32 +1,7 @@
 #!/bin/bash
 
-set -euo pipefail
+mvn clean install
 
-function configureTravis {
-  mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v25 | tar zx --strip-components 1 -C ~/.local
-  source ~/.local/bin/install
-}
-configureTravis
-
-case "$TESTS" in
-
-CI)
-  mvn verify -B -e -V
-  ;;
-
-IT-DEV)
-  mvn package -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true -e -B
-
-  cd its/plugin
-  mvn -Dsonar.runtimeVersion="DEV" -Dmaven.test.redirectTestOutputToFile=false verify -e -B -V
-  ;;
-
-IT-LTS)
-  mvn package -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true -e -B
-
-  cd its/plugin
-  mvn -Dsonar.runtimeVersion="LTS" -Dmaven.test.redirectTestOutputToFile=false verify -e -B
-  ;;
-
-esac
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  mvn sonar:sonar -Dsonar.login=$SONAR_TOKEN
+fi
