e08ae892a519a020543e71ebd60d0e040b476feb
David RACODON <david.racodon@gmail.com>
2016-08-28 19:10:59 +0200

    Add SonarQube in PR

--- travis.sh
+++ travis.sh
@@ -2,9 +2,18 @@
 
 mvn -B clean install
 
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "SQ_VERSION" == "LTS" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$SQ_VERSION" == "LTS" ]; then
+  mvn -B sonar:sonar \
+      -Dsonar.analysis.mode=issues \
+      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+      -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+      -Dsonar.github.oauth=$GITHUB_TOKEN \
+      -Dsonar.login=$SONAR_TOKEN
+
+elif [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$SQ_VERSION" == "LTS" ]; then
   mvn -B sonar:sonar -Dsonar.login=$SONAR_TOKEN
 fi
 
 cd its
 mvn -B clean install -Dsonar.runtimeVersion=$SQ_VERSION
+
