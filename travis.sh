36d6a6da32c4b76398b68e778b3ad93643dcadf4
David RACODON <david.racodon@gmail.com>
2016-08-30 21:00:15 +0200

    Replace issues mode by preview mode

--- travis.sh
+++ travis.sh
@@ -4,7 +4,7 @@ mvn -B clean install
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$SQ_VERSION" == "LTS" ]; then
   mvn -B sonar:sonar \
-      -Dsonar.analysis.mode=issues \
+      -Dsonar.analysis.mode=preview \
       -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
       -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
       -Dsonar.github.oauth=$GITHUB_TOKEN \
