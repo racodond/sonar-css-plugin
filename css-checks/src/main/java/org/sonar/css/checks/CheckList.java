e3cebf9604535a72fd11234037899997a2eabcc6
David RACODON <david.racodon@gmail.com>
2016-09-26 16:08:30 +0200

    Fix #25 New rule: Prefer single-line comments (//) over multi-line (/* ... */) since they're not rendered in the final CSS

--- css-checks/src/main/java/org/sonar/css/checks/CheckList.java
+++ css-checks/src/main/java/org/sonar/css/checks/CheckList.java
@@ -66,6 +66,7 @@ public final class CheckList {
         LessVariableNamingConventionCheck.class,
         ObsoleteNotLessFunctionCheck.class,
         org.sonar.css.checks.less.ParsingErrorCheck.class,
+        PreferSingleLineCommentsCheck.class,
         UnknownLessFunctionCheck.class).stream())
       .collect(Collectors.toList());
   }
