d47bffeac6b6c1cbd45fa08af383706bdc7b3fe9
David RACODON <david.racodon@gmail.com>
2016-09-27 23:50:55 +0200

    Fix SonarQube analysis configuration

--- travis.sh
+++ travis.sh
@@ -4,6 +4,7 @@ mvn -B clean install
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$SQ_VERSION" == "LTS" ]; then
   mvn -B sonar:sonar \
+      -Dsonar.host.url=https://sonarqube.com \
       -Dsonar.analysis.mode=preview \
       -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
       -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
@@ -11,7 +12,7 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$SQ_VERSION" == "LTS" ]; then
       -Dsonar.login=$SONAR_TOKEN
 
 elif [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$SQ_VERSION" == "LTS" ]; then
-  mvn -B sonar:sonar -Dsonar.login=$SONAR_TOKEN
+  mvn -B sonar:sonar -Dsonar.host.url=https://sonarqube.com -Dsonar.login=$SONAR_TOKEN
 fi
 
 cd its
