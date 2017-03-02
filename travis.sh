ec83cac4382b406d8f76734d70ccdf35fc50a4c7
David RACODON <david.racodon@gmail.com>
2017-03-02 11:34:34 +0100

    Remove analysis on SonarQube.com

--- travis.sh
+++ travis.sh
@@ -4,18 +4,18 @@ set -euo pipefail
 
 mvn -B clean install
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$SQ_VERSION" == "LTS" ]; then
-  mvn -B sonar:sonar \
-      -Dsonar.host.url=https://sonarqube.com \
-      -Dsonar.analysis.mode=preview \
-      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
-      -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-      -Dsonar.github.oauth=$GITHUB_TOKEN \
-      -Dsonar.login=$SONAR_TOKEN
-
-elif [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$SQ_VERSION" == "LTS" ]; then
-  mvn -B sonar:sonar -Dsonar.host.url=https://sonarqube.com -Dsonar.login=$SONAR_TOKEN
-fi
+#if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$SQ_VERSION" == "LTS" ]; then
+#  mvn -B sonar:sonar \
+#      -Dsonar.host.url=https://sonarqube.com \
+#      -Dsonar.analysis.mode=preview \
+#      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+#      -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+#      -Dsonar.github.oauth=$GITHUB_TOKEN \
+#      -Dsonar.login=$SONAR_TOKEN
+#
+#elif [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$SQ_VERSION" == "LTS" ]; then
+#  mvn -B sonar:sonar -Dsonar.host.url=https://sonarqube.com -Dsonar.login=$SONAR_TOKEN
+#fi
 
 cd its
 mvn -B clean install -Dsonar.runtimeVersion=$SQ_VERSION
