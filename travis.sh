9629f9a56e28d1c4d96b5ea1c14053eb2991e307
David RACODON <david.racodon@gmail.com>
2017-06-15 11:40:28 +0200

    Add SonarQube analysis on sonarcloud.io

--- travis.sh
+++ travis.sh
@@ -4,6 +4,18 @@ set -euo pipefail
 
 mvn -B clean install
 
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$SQ_VERSION" == "LTS" ]; then
+  mvn -B sonar:sonar \
+      -Dsonar.host.url=https://sonarcloud.io \
+      -Dsonar.analysis.mode=preview \
+      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+      -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+      -Dsonar.github.oauth=$GITHUB_TOKEN \
+      -Dsonar.login=$SONAR_TOKEN
+
+elif [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$SQ_VERSION" == "LTS" ]; then
+  mvn -B sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN
+fi
+
 cd its
 mvn -B clean install -Dsonar.runtimeVersion=$SQ_VERSION
-
