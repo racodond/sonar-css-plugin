05654837e6fef90c6d1ba2810856073da515af9b
David RACODON <david.racodon@gmail.com>
2017-06-15 12:01:36 +0200

    Add SonarQube badge

--- travis.sh
+++ travis.sh
@@ -11,11 +11,17 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$SQ_VERSION" == "LTS" ]; then
       -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
       -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
       -Dsonar.github.oauth=$GITHUB_TOKEN \
-      -Dsonar.login=$SONAR_TOKEN
+      -Dsonar.login=$SONAR_TOKEN \
+      -Dsonar.organization=racodond-github
 
 elif [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$SQ_VERSION" == "LTS" ]; then
-  mvn -B sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN
+  mvn -B sonar:sonar \
+      -Dsonar.host.url=https://sonarcloud.io \
+      -Dsonar.login=$SONAR_TOKEN \
+      -Dsonar.organization=racodond-github
 fi
 
 cd its
-mvn -B clean install -Dsonar.runtimeVersion=$SQ_VERSION
+
+mvn -B clean install \
+    -Dsonar.runtimeVersion=$SQ_VERSION
