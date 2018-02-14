e74c190ae6b02cb0064338277aff82dd10c1e639
David RACODON <david.racodon@gmail.com>
2018-02-14 18:44:24 +0100

    Fix travis

--- travis.sh
+++ travis.sh
@@ -6,19 +6,18 @@ mvn -B clean install -Pits -Dsonar.runtimeVersion=$SQ_VERSION
 
 if [ "$SQ_VERSION" == "LTS" ]; then
 
-    mvnCommand = "mvn -B sonar:sonar"
-    commonArgs = "-Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN -Dsonar.organization=racodond-github"
-    githubAgs = "-Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST -Dsonar.github.repository=$TRAVIS_REPO_SLUG -Dsonar.github.oauth=$GITHUB_TOKEN"
-    branchArgs = ''
+    mvnCommand='mvn -B sonar:sonar'
+    commonArgs="-Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN -Dsonar.organization=racodond-github"
+    githubArgs="-Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST -Dsonar.github.repository=$TRAVIS_REPO_SLUG -Dsonar.github.oauth=$GITHUB_TOKEN"
+    branchArgs=''
 
     if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         if [ "$TRAVIS_BRANCH" != "master" ]; then
            branchArgs = "-Dsonar.branch.name=$TRAVIS_BRANCH -Dsonar.branch.target=master"
         fi
-        $mvnCommand $commonArgs $branchArgs
+        eval $mvnCommand $commonArgs $branchArgs
     else
-        $mvnCommand $commonArgs $githubArgs
-
+        eval $mvnCommand $commonArgs $githubArgs
     fi
 
 fi
