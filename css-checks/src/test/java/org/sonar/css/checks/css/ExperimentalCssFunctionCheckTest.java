9aa2b128f78b6443a97d3f88f22960a2f2f45f78
David RACODON <david.racodon@gmail.com>
2018-04-25 15:48:15 +0200

    Fix #102 Add parameter to 'experimental-function-usage' rules to flag some functions as non-experimental

--- css-checks/src/test/java/org/sonar/css/checks/css/ExperimentalCssFunctionCheckTest.java
+++ css-checks/src/test/java/org/sonar/css/checks/css/ExperimentalCssFunctionCheckTest.java
@@ -21,13 +21,35 @@ package org.sonar.css.checks.css;
 
 import org.junit.Test;
 import org.sonar.css.checks.CheckTestUtils;
+import org.sonar.css.checks.common.ExperimentalPseudoCheck;
 import org.sonar.css.checks.verifier.CssCheckVerifier;
+
+import static org.fest.assertions.Assertions.assertThat;
 
 public class ExperimentalCssFunctionCheckTest {
 
   @Test
   public void test() {
     CssCheckVerifier.verifyCssFile(new ExperimentalCssFunctionCheck(), CheckTestUtils.getCssTestFile("experimentalFunctionUsage.css"));
+  }
+
+  @Test
+  public void test_exclude_functions() {
+    ExperimentalCssFunctionCheck check = new ExperimentalCssFunctionCheck();
+    check.setFunctionsToExclude("conic-gradient|count.*");
+    CssCheckVerifier.verifyCssFile(check, CheckTestUtils.getCssTestFile("experimentalFunctionUsageExcludeFunctions.css"));
+  }
+
+  @Test
+  public void should_throw_an_illegal_state_exception_as_the_functionsToExclude_parameter_is_not_valid() {
+    try {
+      ExperimentalCssFunctionCheck check = new ExperimentalCssFunctionCheck();
+      check.setFunctionsToExclude("(");
+      CssCheckVerifier.issuesOnCssFile(check, CheckTestUtils.getCssTestFile("experimentalFunctionUsage.css")).noMore();
+    } catch (IllegalStateException e) {
+      assertThat(e.getMessage()).isEqualTo("Check css:experimental-function-usage (Experimental functions should not be used): "
+        + "functionsToExclude parameter \"(\" is not a valid regular expression.");
+    }
   }
 
 }
