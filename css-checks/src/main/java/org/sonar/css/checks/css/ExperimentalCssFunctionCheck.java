20cb165af0d55eef6ade2a8484ef1853b5962793
David RACODON <david.racodon@gmail.com>
2018-04-25 15:48:15 +0200

    Fix #102 Add parameter to 'experimental-function-usage' rules to flag some functions as non-experimental

--- css-checks/src/main/java/org/sonar/css/checks/css/ExperimentalCssFunctionCheck.java
+++ css-checks/src/main/java/org/sonar/css/checks/css/ExperimentalCssFunctionCheck.java
@@ -19,13 +19,20 @@
  */
 package org.sonar.css.checks.css;
 
+import com.google.common.annotations.VisibleForTesting;
 import org.sonar.check.Priority;
 import org.sonar.check.Rule;
+import org.sonar.check.RuleProperty;
+import org.sonar.css.checks.CheckList;
+import org.sonar.css.checks.CheckUtils;
 import org.sonar.css.checks.Tags;
 import org.sonar.plugins.css.api.tree.css.FunctionTree;
 import org.sonar.plugins.css.api.visitors.DoubleDispatchVisitorCheck;
 import org.sonar.squidbridge.annotations.ActivatedByDefault;
 import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
+
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 @Rule(
   key = "experimental-function-usage",
@@ -36,14 +43,46 @@ import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
 @ActivatedByDefault
 public class ExperimentalCssFunctionCheck extends DoubleDispatchVisitorCheck {
 
+  private static final String DEFAULT_FUNCTIONS_TO_EXCLUDE = "";
+
+  @RuleProperty(
+    key = "functionsToExclude",
+    description = "A regular expression to exclude functions from being considered as experimental. Example: \"conic-gradient|count.*\". See " + CheckUtils.LINK_TO_JAVA_REGEX_PATTERN_DOC + " for detailed regular expression syntax.",
+    defaultValue = DEFAULT_FUNCTIONS_TO_EXCLUDE)
+  private String functionsToExclude = DEFAULT_FUNCTIONS_TO_EXCLUDE;
+
   @Override
   public void visitFunction(FunctionTree tree) {
-    if (tree.standardFunction().isCss() && (tree.isVendorPrefixed() || tree.standardFunction().isExperimental())) {
+    if (tree.standardFunction().isCss()
+      && !tree.standardFunction().getName().matches(functionsToExclude)
+      && (tree.isVendorPrefixed() || tree.standardFunction().isExperimental())) {
       addPreciseIssue(
         tree.function(),
         "Remove this usage of the experimental \"" + tree.standardFunction().getName() + "\" function.");
     }
     super.visitFunction(tree);
+  }
+
+
+  @Override
+  public void validateParameters() {
+    try {
+      Pattern.compile(functionsToExclude);
+    } catch (PatternSyntaxException exception) {
+      throw new IllegalStateException(paramsErrorMessage(), exception);
+    }
+  }
+
+  @VisibleForTesting
+  void setFunctionsToExclude(String functionsToExclude) {
+    this.functionsToExclude = functionsToExclude;
+  }
+
+  private String paramsErrorMessage() {
+    return CheckUtils.paramsErrorMessage(
+      this.getClass(),
+      CheckList.CSS_REPOSITORY_KEY,
+      "functionsToExclude parameter \"" + functionsToExclude + "\" is not a valid regular expression.");
   }
 
 }
