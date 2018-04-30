60b92e9249f7537b5135d7979333d2aff36f7c57
David RACODON <david.racodon@gmail.com>
2018-04-30 10:50:25 +0200

    Fix #105 Support CSS Fonts Module Level 4 - font-variation-settings

---
+++ css-frontend/src/main/java/org/sonar/css/model/property/standard/FontVariationSettings.java
@@ -0,0 +1,34 @@
+/*
+ * SonarQube CSS / SCSS / Less Analyzer
+ * Copyright (C) 2013-2017 David RACODON
+ * mailto: david.racodon@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.css.model.property.standard;
+
+import org.sonar.css.model.property.StandardProperty;
+import org.sonar.css.model.property.validator.property.FontVariationSettingsValidator;
+
+public class FontVariationSettings extends StandardProperty {
+
+  public FontVariationSettings() {
+    setExperimental(true);
+    addLinks("https://drafts.csswg.org/css-fonts-4/#descdef-font-face-font-variation-settings",
+      "https://developer.mozilla.org/en-US/docs/Web/CSS/font-variation-settings");
+    addValidators(new FontVariationSettingsValidator());
+  }
+
+}
